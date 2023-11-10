025eec751332e3976939cff52341e398c93befbe
danny <sjia00573@student.monash.edu>
2023-11-10 23:00:54 +1100

    mkdir

--- video.py
+++ video.py
@@ -2,20 +2,8 @@ import requests
 import subprocess
 import os 
 
-
 text = """
-#EXTM3U
-#EXT-X-VERSION:3
-#EXT-X-PLAYLIST-TYPE:VOD
-#EXT-X-MEDIA-SEQUENCE:0
-#EXT-X-TARGETDURATION:9
-#EXTINF:4.170833,
-https://0000000.ts
-#EXTINF:6.172833,
-https://0000001.ts
-#EXTINF:2.627622,
-https://0000002.ts
-#EXT-X-ENDLIST
+
 """
 
 
@@ -25,6 +13,8 @@ playlist_lines = text.split('\n')
 
 ts_urls = [line for line in playlist_lines if line.endswith('.ts')]
 
+command = f'mkdir tsfiles'
+subprocess.run(command, shell=True)
 for i, ts_url in enumerate(ts_urls):
     ts_response = requests.get(ts_url)
     with open(f"tsfiles/tssegment_{i}.ts", 'wb') as ts_file:
